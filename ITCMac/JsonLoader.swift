0dbf61aafa1767d5d6d7a1596a867c930896069c
adambunim <adambunim@gmail.com>
2023-08-06 18:18:41 +0300

    load from api

--- ITCMac/JsonLoader.swift
+++ ITCMac/JsonLoader.swift
@@ -1,7 +1,7 @@
 
 import Foundation
 
-func loadJson() -> [FeedItem] {
+func loadJsonFromFile() -> [FeedItem] {
     guard let filepath = Bundle.main.path(forResource: "items", ofType: "json") else {
         return []
     }
@@ -12,4 +12,27 @@ func loadJson() -> [FeedItem] {
     } catch {
         return []
     }
+}
+
+func loadDummyJson() -> [FeedItem] {
+    return [
+        FeedItem(id: "1", title: "milk", body: "milk"),
+                 FeedItem(id: "2", title: "cookies", body: "cookies")
+    ]
+}
+
+func loadFromApi(_ callback: @escaping ([FeedItem]) -> Void) {
+    guard let url = URL(string: "http://localhost:3000") else {
+        callback([])
+        return
+    }
+
+    let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+        guard let data = data else {
+            callback([])
+            return
+        }
+        print(String(data: data, encoding: .utf8)!)
+    }
+    task.resume()
 }
--- ITCMac/views/ContentView.swift
+++ ITCMac/views/ContentView.swift
@@ -3,26 +3,44 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var loading = false
     @State var items: [FeedItem]? = nil
     
     var body: some View {
-        if let items = items {
-            ItemsList(items: items)
-        }
-        else {
-            ProgressView()
-                .onAppear {
+        VStack {
+            HStack {
+                Spacer()
+                Text("Messages")
+                    .font(.title)
+                Spacer()
+                
+                Button(action: {
                     load()
+                }) {
+                    Image(systemName: "arrow.triangle.2.circlepath")
                 }
+                .buttonStyle(.plain)
+                .disabled(loading)
+            }
+            .padding()
+            
+            if let items = items {
+                ItemsList(items: items)
+            }
+            else {
+                ProgressView()
+                    .onAppear {
+                        load()
+                    }
+            }
         }
     }
     
     func load() {
-            items = loadJson()
-        
-//            items = [
-//                FeedItem(id: "1", title: "milk", body: "milk"),
-//                         FeedItem(id: "2", title: "cookies", body: "cookies")
-//            ]
+        loading = true
+        loadFromApi { response in
+            loading = false
+            items = response
+        }
     }
 }
