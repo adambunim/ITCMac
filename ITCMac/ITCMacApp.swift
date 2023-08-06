ec342ad5f079c907f001f42c94fcc6c1e1da0758
adambunim <adambunim@gmail.com>
2023-08-06 17:21:28 +0300

    Initial Commit

---
+++ ITCMac/ITCMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  ITCMacApp.swift
+//  ITCMac
+//
+//  Created by unim on 06/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ITCMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
