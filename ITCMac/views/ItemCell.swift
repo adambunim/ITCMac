e73315650cc499e8f0f2a54fda270340f44e2174
adambunim <adambunim@gmail.com>
2023-08-08 10:03:50 +0300

    more

--- ITCMac/views/ItemCell.swift
+++ ITCMac/views/ItemCell.swift
@@ -7,45 +7,52 @@ struct ItemCell: View {
     @State private var showingSheet = false
     
     var body: some View {
-        Button(action: {
-            showingSheet = true
-        }) {
-            HStack {
-                VStack(alignment: .leading, spacing: 10) {
-                    Text(item.title)
-                        .font(.title)
-                        
-                    Text(item.details)
-                        .font(.body)
-                    
-                    HStack(spacing: 0) {
-                        Spacer()
-                        
-                        Button(action: {
-                            print("more")
-                        }) {
-                            Text("more")
-                        }
-                        .buttonStyle(.plain)
-                        
-                        Image(systemName: "chevron.down")
-                    }
-                    .foregroundColor(.accentColor)
-                    
-                }
+        VStack(alignment: .leading, spacing: 10) {
+            Text(item.title)
+                .font(.title)
+               
+            if showingSheet {
+                Text(item.details)
+                    .font(.body)
+            }
+            
+            HStack(spacing: 0) {
                 Spacer()
+                
+                if showingSheet {
+                    Button(action: {
+                        showingSheet = false
+                    }) {
+                        Text("less")
+                    }
+                    .buttonStyle(.plain)
+                    
+                    Image(systemName: "chevron.up")
+                }
+                else {
+                    Button(action: {
+                        showingSheet = true
+                    }) {
+                        Text("more")
+                    }
+                    .buttonStyle(.plain)
+                    
+                    Image(systemName: "chevron.down")
+                }
             }
-            .padding(.leading, 20)
-            .padding(.trailing, 20)
-            .padding(.top, 8)
-            .padding(.bottom, 8)
-            .background(Color.white)
-            .cornerRadius(10)
-            .sheet(isPresented: $showingSheet) {
-                ItemView(item: item, showingSheet: $showingSheet)
-            }
+            .foregroundColor(.accentColor)
+            
         }
-        .buttonStyle(.plain)
+        .padding(.leading, 20)
+        .padding(.trailing, 20)
+        .padding(.top, 8)
+        .padding(.bottom, 8)
+        .background(Color.white)
+        .cornerRadius(10)
+//        .sheet(isPresented: $showingSheet) {
+//            ItemView(item: item, showingSheet: $showingSheet)
+//        }
+//        .buttonStyle(.plain)
     }
 }
 
